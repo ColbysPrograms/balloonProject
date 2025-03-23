5ef49b9c0e7678c49b544b6aeb21533aabf57864
Colby Cox <colby.cox2020@gmail.com>
2025-03-23 18:05:18 -0500

    Added balloon model

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-//import(OrbitControls) from 'three/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -12,7 +12,9 @@ renderer.render(scene, camera);
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 
-camera.position.set(0, 2, 5);
+camera.position.set(50, 15, 5);
+
+const controls = new OrbitControls(camera, renderer.domElement);
 
 const cylinderGeometry = new THREE.CylinderGeometry(5, 5, 5);
 const cylinderMaterial = new THREE.MeshBasicMaterial({ color: 0xFF6347 });
@@ -20,21 +22,29 @@ const cylinder = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
 
 scene.add(cylinder)
 
-const coneGeometry = new THREE.CylinderGeometry(1, 5, 5);
+const coneGeometry = new THREE.CylinderGeometry(0.2, 5, 5);
 const cone = new THREE.Mesh(coneGeometry, cylinderMaterial);
-cone.position.set(0, 0, 5)
+cone.position.set(0, 5, 0)
 scene.add(cone);
 
-const ambientLight = new THREE.AmbientLight(0x404040)
-scene.add(ambientLight)
+const sphereGeometry = new THREE.SphereGeometry(5);
+const sphereMaterial = new THREE.MeshStandardMaterial({ color: 0x0000FF });
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+sphere.position.set(0, 12, 0);
 
-const pointLight = new THREE.PointLight(0xffffff, 100)
-pointLight.position.set(0, 0, 10)
+scene.add(sphere);
+
+const ambientLight = new THREE.AmbientLight(0x404040);
+scene.add(ambientLight);
+
+const pointLight = new THREE.PointLight(0xffffff, 100);
+pointLight.position.set(0, 0, 0);
 
 scene.add(pointLight)
 
 const animate = function () {
   requestAnimationFrame(animate);
+  controls.update();
   renderer.render(scene, camera);
 };
 
