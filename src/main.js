d593e7772f59d31970d230e9f01eedf01bbc3f41
Colby Cox <colby.cox2020@gmail.com>
2025-04-08 00:08:29 -0500

    Finalized Data Portion
    This may be the final commit, unless I have time to add balloon popping
    need to fix something

--- src/main.js
+++ src/main.js
@@ -22,8 +22,8 @@ labelRenderer.domElement.style.top = '0px';
 labelRenderer.domElement.style.pointerEvents = 'none';
 document.body.appendChild(labelRenderer.domElement);
 
-const axesHelper = new THREE.AxesHelper(20);
-scene.add(axesHelper);
+//const axesHelper = new THREE.AxesHelper(20);
+//scene.add(axesHelper);
 
 const cylinderGeometry = new THREE.CylinderGeometry(5, 5, 5);
 const cylinderMaterial = new THREE.MeshStandardMaterial({ color: 0xFF6347 });
@@ -47,6 +47,12 @@ const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 cube.position.set(0, -5, 0);
 scene.add(cube);
 
+//const testSphereGeometry = new THREE.SphereGeometry(10);
+//const testSphereMaterial = new THREE.MeshBasicMaterial({ color: 0xFF0000, wireframe: true });
+//const testSphere = new THREE.Mesh(testSphereGeometry, testSphereMaterial);
+//testSphere.position.set(0, 12, 0);
+//scene.add(testSphere);
+
 const ambientLight = new THREE.AmbientLight(0x999999);
 scene.add(ambientLight);
 
@@ -54,32 +60,48 @@ const pointLight = new THREE.PointLight(0xFFFFFF, 100);
 pointLight.position.set(10, 15, 5);
 scene.add(pointLight);
 
-const center = new THREE.Vector3(0, 12, 0);
-const end = new THREE.Vector3(0, 17, 0);
-var radius = center.distanceTo(end);
 scene.background = new THREE.Color(0x1264AC);
+
+var radius = sphere.scale.y * 5;
 
 const gui = new dat.GUI();
 var radiusChange = { radiusChange: 0 };
 gui.add(radiusChange, "radiusChange", -0.01, 0.01, 0.001);
 
-const drdt = document.createElement('p');
-drdt.className = 'derivative';
-drdt.textContent = 'dr/dt = ' + radiusChange.radiusChange;
-const dvdt = document.createElement('p');
-dvdt.className = 'derivative';
-dvdt.textContent = 'dV/dt = ' + radius;
-const dvdr = document.createElement('p');
-dvdr.className = 'derivative';
-dvdr.textContent = 'dV/dr = ';
+const drdtText = document.createElement('p');
+drdtText.className = 'derivative';
+drdtText.textContent = 'dr/dt = ' + radiusChange.radiusChange;
+const dVdtText = document.createElement('p');
+dVdtText.className = 'derivative';
+dVdtText.textContent = 'dV/dt =';
+const dVdrText = document.createElement('p');
+dVdrText.className = 'derivative';
+dVdrText.textContent = 'dV/dr = ';
+const volumeText = document.createElement('p');
+volumeText.className = 'derivative';
+volumeText.textContent = 'Volume = ';
+const scaleText = document.createElement('p');
+scaleText.className = 'derivative';
+scaleText.textContent = 'Scale = ';
+const radiusText = document.createElement('p');
+radiusText.className = 'derivative';
+radiusText.textContent = 'Radius = ';
+
 const pContainer = document.createElement('div');
-pContainer.appendChild(drdt);
-pContainer.appendChild(dvdt);
-pContainer.appendChild(dvdr);
+pContainer.appendChild(scaleText);
+pContainer.appendChild(radiusText);
+pContainer.appendChild(volumeText);
+pContainer.appendChild(drdtText);
+pContainer.appendChild(dVdtText);
+pContainer.appendChild(dVdrText);
 const cPointLabel = new CSS2DObject(pContainer);
 cPointLabel.position.set(20, 1, 20);
 scene.add(cPointLabel);
 
+var pi = 3.14159265;
+var dVdr = 0;
+var dVdt = 0;
+var volume = 0;
 
 const animate = function() {
   requestAnimationFrame(animate);
@@ -90,16 +112,24 @@ const animate = function() {
     sphere.scale.setScalar(sphere.scale.x + radiusChange.radiusChange);
     //sphere.scale.set(sphere.scale.x + radiusChange.radiusChange, sphere.scale.y + radiusChange.radiusChange, sphere.scale.z + radiusChange.radiusChange);
     sphere.position.set(sphere.position.x, sphere.position.y + 5 * radiusChange.radiusChange, sphere.position.z);
+    //sphere.position.y.set(sphere.position.y + 5 * radiusChange.radiusChange);
   }
   else {
     sphere.scale.set(1, 1, 1);
     sphere.position.set(0, 12, 0);
   }
-  end.y += radiusChange.radiusChange;
-  radius = center.distanceTo(end);
-  drdt.textContent = 'dr/dt = ' + radiusChange.radiusChange;
-  dvdt.textContent = 'dV/dt = ' + radius;
+  radius = sphere.scale.x * 5;
+  volume = 4 / 3 * pi * Math.pow(radius, 3);
+  dVdt = 4 * pi * Math.pow(radius, 2) * radiusChange.radiusChange;
+  dVdr = 4 * pi * Math.pow(radius, 2);
+  scaleText.textContent = 'Scale = ' + sphere.scale.x.toPrecision(3);
+  radiusText.textContent = 'Radius = ' + radius.toPrecision(3);
+  drdtText.textContent = 'dr/dt = ' + (radiusChange.radiusChange * 5).toPrecision(2);
+  volumeText.textContent = 'Volume = ' + volume.toPrecision(6);
+  dVdtText.textContent = 'dV/dt = ' + dVdt.toPrecision(3);
+  dVdrText.textContent = 'dV/dr = ' + dVdr.toPrecision(6);
 };
+
 //window.addEventListener('resize', function() {
 //  camera.aspect = window.innerWidth / window.innerHeight;
 //  camera.updateProjectionMatrix();
