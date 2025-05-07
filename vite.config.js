82b3a476f8f8cbcbd8da96d1d5258b97ff618ffc
Colby <colby.cox2020@gmail.com>
2025-05-06 20:32:55 -0500

    Fixed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/balloonProject/' : ''
-  //server: {
-  //  port: '5173',
-  //  allowedHosts: ['balloon.colbysserver.com'],
-  //},
+  base: '/balloonProject/'
 })
